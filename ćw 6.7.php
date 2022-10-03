a555fe0438dc1d2a41d3d053e331cab5eeabed7a
hope <hope@hope.hope>
2022-10-03 10:19:54 +0200

    zmiany nazw

--- ćw 6.7.php
+++ ćw 6.7.php
@@ -1,43 +1,9 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-    <title>tabliczka mnożenia</title>
-    <meta charset="utf-8">
-    <style>
-        table {
-            width: 400px
-        }
-        table, tr, td, th {
-            border: 1px solid;
-            border-collapse: collapse;
-            text-align: center;
-        }
-    </style>
-    </head>
-<body>
-    <table>
-    <tr>
-    <th></th>
 <?php
-$wiersz = 10;
-$kolumna = 10;
-for ($i = 1; $i <= $kolumna; $i++){
-    echo "<th>" . $i . "</th>";
-}
-?>
-</tr>
-<?php
-for ($i = 1; $i <= $wiersz; $i++){
-    echo "<tr>";
-    echo "<th>". $i . "</th>";
-    for ($j = 1; $j <= $kolumna; $j++) {
-        echo "<td>";
-        echo $i * $j;
-        echo "</td>";
+$tab = array(0,1,2,3,4,5,6,7,8,9,10);
+echo "Liczby parzyste: <br>";
+foreach($tab as $i) {
+    if($i%2==0) {
+    echo("$i \n");    
     }
-    echo "</tr>";
 }
 ?>
-</table>
-</body>
-</html>
