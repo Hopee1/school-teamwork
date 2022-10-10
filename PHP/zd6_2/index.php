ac7f6cb391b2f4fd726aa553d15870c060486474
halik <halik@halik.pl>
2022-10-10 11:39:57 +0200

    zd6_2

---
+++ PHP/zd6_2/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+
+    <div class="header">
+        <div class="side-nav">
+            <a href="#" class="logo" >
+                <img src="logo.png" class="logo" >
+            </a>
+            <ul class>
+                <li><a href="martykul.php"><p>Moje artykuły</p></a></li>
+            </ul>
+        </div>
+    </div>
+        <?php 
+        //https://www.youtube.com/watch?v=Fx_NpAPM-iU
+
+        ?>
+    </body>
+</html>
