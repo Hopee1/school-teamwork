30b66af9936052fc34a7349c63a5fd2ade3b0a83
hope <hope@hope.hope>
2022-10-10 11:09:49 +0200

    a

--- zd6_1/clear.php
+++ zd6_1/clear.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 
 <html>
-<body onload="href="form.php"">
+<META HTTP-EQUIV="Refresh" CONTENT="1; URL=form.php">
+ <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+<head>
+	
+</head>
+<body>
+    <center><h1>USUWANIE</h1></center>
 <?php 
     file_put_contents('opinie.txt', '');
+    
 ?>
+
 </body>
 </html>
