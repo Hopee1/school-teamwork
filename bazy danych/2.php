88f2c35ad57f6a3fd1cc654b964331389bc05feb
hope <hope@hope.hope>
2022-11-04 09:44:42 +0100

    4.11 druga lekcja

--- bazy danych/2.php
+++ bazy danych/2.php
@@ -1,5 +1,4 @@
 <!DOCTYPE HTML>
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,12 +9,17 @@
 <body>
     <?php
     // login.php
+    echo "test1";
     $db_hostname = 'localhost';
-    $db_database = 'publications';
-    $db_username = 'username';
-    $db_password = 'password';
+    $db_database = 'sakila';
+    $db_username = 'root';
+    $db_password = '';
     $db_server = mysql_connect($db_hostname, $db_username, $db_password);
     if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
+
+    $query = mysqli_query("$host","$user","$pass","$db");
+    $numrows = mysqli_num_rows($query);
+    echo "amount of tables:".$numrows."and their names </br>";
     ?> 
 </body>
 </html>
