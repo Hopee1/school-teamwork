50343eb4a3098daf2c9af112db3ab078b9440108
hope <hope@hope.hope>
2022-11-10 14:29:29 +0100

    12323

---
+++ ćw 6.19.php
@@ -0,0 +1,25 @@
+<?php
+if (!isset($_COOKIE ['odwiedz'])) {
+$odw = 1;
+} else {
+$odw = intval($_COOKIE['odwiedz']) + 1;
+}
+setcookie("odwiedz", $odw, time()+60*60*24*365);
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Odwiedziny</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<?php
+if ($odw == 1) {
+$wyraz = "raz";
+} else {
+$wyraz = "razy";
+}
+echo "W ciągu ostatniego roku odwiedziłeś naszą stronę $odw $wyraz.<br>";
+?>
+</body>
+</html>
